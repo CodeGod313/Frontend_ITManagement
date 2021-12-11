e458849901942ec7c1f0a3ccf6662502dd564b18
Ales <codegod313@gmail.com>
2021-12-11 06:11:04 +0300

    OrderCreation now creates orders

--- src/by/vita02/frontend/controllers/OrderCreationController.java
+++ src/by/vita02/frontend/controllers/OrderCreationController.java
@@ -1,123 +1,155 @@
 package by.vita02.frontend.controllers;
 
+import by.vita02.frontend.connection.SocketService;
+import by.vita02.frontend.dto.QueryDTO;
+import by.vita02.frontend.services.LastQueryService;
+import by.vita02.frontend.stageConfig.StageConfig;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
+
+import java.io.IOException;
 
 public class OrderCreationController {
 
-    @FXML
-    private Text RadioValidationField;
+  public static final String EMPTY_STRING = "";
+  @FXML private Text RadioValidationField;
 
-    @FXML
-    private Button calculateButton;
+  @FXML private Button cancelButton;
 
-    @FXML
-    private Button calculateButton1;
+  @FXML private Button checkoutButton;
 
-    @FXML
-    private RadioButton corpSiteRadio;
+  @FXML private RadioButton corpSiteRadio;
 
-    @FXML
-    private RadioButton internetShopRadio;
+  @FXML private RadioButton internetShopRadio;
 
-    @FXML
-    private RadioButton mobileAppRadio;
+  @FXML private RadioButton mobileAppRadio;
 
-    @FXML
-    private TextField quantityOfConventionalUnitsTextField;
+  @FXML private TextField quantityOfConventionalUnitsTextField;
 
-    @FXML
-    private Text quantityOfConventionalUnitsValidationField;
+  @FXML private Text quantityOfConventionalUnitsValidationField;
 
-    @FXML
-    private RadioButton siteCatalogRadio;
+  @FXML private RadioButton siteCatalogRadio;
 
-    @FXML
-    private TextField targetTextField;
+  @FXML private TextField targetTextField;
 
-    @FXML
-    private Text targetValidationField;
+  @FXML private Text targetValidationField;
 
-    @FXML
-    private RadioButton visitCardRadio;
+  @FXML private Text totalPriceField;
 
-    @FXML
-    private Text totalPriceField;
+  @FXML private RadioButton visitCardRadio;
 
-    @FXML
-    void initialize(){
-        visitCardRadio.setOnAction(actionEvent -> {
-            if(!mobileAppRadio.isDisable()){
-                mobileAppRadio.fire();
+  @FXML
+  void initialize() {
+    ToggleGroup toggleGroupRadio = new ToggleGroup();
+    corpSiteRadio.setToggleGroup(toggleGroupRadio);
+    internetShopRadio.setToggleGroup(toggleGroupRadio);
+    mobileAppRadio.setToggleGroup(toggleGroupRadio);
+    siteCatalogRadio.setToggleGroup(toggleGroupRadio);
+    visitCardRadio.setToggleGroup(toggleGroupRadio);
+    checkoutButton.setOnAction(
+        actionEvent -> {
+          if (!validation(toggleGroupRadio)) {
+            return;
+          }
+          String totalPriceString = totalPriceField.getText().substring(0, totalPriceField.getText().length() - 2);
+          String count = quantityOfConventionalUnitsTextField.getText();
+          String target = targetTextField.getText();
+          JsonObject itProject = new JsonObject();
+          String projectType = ((RadioButton) toggleGroupRadio.getSelectedToggle()).getText();
+          switch (projectType){
+            case "1 - Сайт-визитка 100$"-> {
+              itProject.addProperty("projectType", "BUSINESS_CARD_SITE");
+              itProject.addProperty("costOfConventionalUnit", "100");
             }
-            if(!corpSiteRadio.isDisable()){
-                mobileAppRadio.fire();
+            case "2 - Мобильное приложение 250$"-> {
+              itProject.addProperty("projectType", "MOBILE_APP");
+              itProject.addProperty("costOfConventionalUnit", "250");
             }
-            if(!internetShopRadio.isDisable()){
-                mobileAppRadio.fire();
+            case "3 - Корпоративный сайт 200$"-> {
+              itProject.addProperty("projectType", "CORPORATE_SITE");
+              itProject.addProperty("costOfConventionalUnit", "200");
             }
-            if(!siteCatalogRadio.isDisable()){
-                mobileAppRadio.fire();
+            case "4 - Интернет-магазин 300$"->{
+              itProject.addProperty("projectType", "ONLINE_SHOP");
+              itProject.addProperty("costOfConventionalUnit", "300");
             }
+            case "5 - Сайт-каталог 80$" ->{
+              itProject.addProperty("projectType", "SITE_CATALOG");
+              itProject.addProperty("costOfConventionalUnit", "80");
+            }
+          }
+          itProject.addProperty("nameOfConventionalUnit", "");
+          JsonObject order = new JsonObject();
+          order.addProperty("cost", totalPriceString);
+          order.addProperty("count", count);
+          order.add("itProject",itProject);
+          QueryDTO queryDTO = new QueryDTO(LastQueryService.getUserId(), "findById");
+          Gson gson = new Gson();
+          try {
+            SocketService.writeLine(gson.toJson(queryDTO));
+            JsonObject client = gson.fromJson(SocketService.readLine(), JsonObject.class);
+            order.addProperty(
+                    "companyName", client.get("company").getAsJsonObject().get("name").getAsString());
+            JsonArray jsonArray = client.get("orders").getAsJsonArray();
+            jsonArray.add(order);
+            client.remove("orders");
+            client.add("orders", jsonArray);
+            QueryDTO queryDTO1 = new QueryDTO(LastQueryService.getUserId(), "updateClientDate");
+            SocketService.writeLine(gson.toJson(queryDTO1));
+            SocketService.writeLine(client.toString());
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/Orders.fxml"));
+            StageConfig.stage.setScene(new Scene(root, 800, 450));
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
         });
-        corpSiteRadio.setOnAction(actionEvent -> {
-            if(!mobileAppRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!visitCardRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!internetShopRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!siteCatalogRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-        });
-        mobileAppRadio.setOnAction(actionEvent -> {
-            if(!visitCardRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!corpSiteRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!internetShopRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!siteCatalogRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-        });
-        internetShopRadio.setOnAction(actionEvent -> {
-            if(!mobileAppRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!corpSiteRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!visitCardRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!siteCatalogRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-        });
-        siteCatalogRadio.setOnAction(actionEvent -> {
-            if(!mobileAppRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!corpSiteRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!internetShopRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-            if(!visitCardRadio.isDisable()){
-                mobileAppRadio.fire();
-            }
-        });
+    quantityOfConventionalUnitsTextField.setOnAction(actionEvent -> {
+        if(!quantityOfConventionalUnitsTextField.getText().matches("\\d+") || toggleGroupRadio.getSelectedToggle() == null){
+            return;
+        }
+        String projectType = ((RadioButton) toggleGroupRadio.getSelectedToggle()).getText();
+        Long costOfConvUnit = 0L;
+        switch (projectType){
+            case "1 - Сайт-визитка 100$"-> costOfConvUnit = 100L;
+            case "2 - Мобильное приложение 250$"-> costOfConvUnit = 250L;
+            case "3 - Корпоративный сайт 200$"-> costOfConvUnit = 200L;
+            case "4 - Интернет-магазин 300$"-> costOfConvUnit = 300L;
+            case "5 - Сайт-каталог 80$" -> costOfConvUnit = 80L;
+        }
+        totalPriceField.setText(String.valueOf(costOfConvUnit * Long.parseLong(quantityOfConventionalUnitsTextField.getText())) + " $");
+    });
+  }
+
+  boolean validation(ToggleGroup group) {
+    boolean result = true;
+    if (group.getSelectedToggle() == null) {
+      RadioValidationField.setText("Выберите проект");
+      result = false;
+    } else {
+      RadioValidationField.setText(EMPTY_STRING);
     }
+    if(!quantityOfConventionalUnitsTextField.getText().matches("\\d+")){
+      quantityOfConventionalUnitsValidationField.setText("Введите число");
+      result = false;
+    }else {
+      quantityOfConventionalUnitsValidationField.setText(EMPTY_STRING);
+    }
+    if(!targetTextField.getText().matches("[A-Z][a-z]+")){
+      targetValidationField.setText("Введите цель");
+      result = false;
+    }else {
+      targetValidationField.setText(EMPTY_STRING);
+    }
+    return result;
+  }
 }
